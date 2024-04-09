377e5a64e0e5f355b985ceb4c08969d61eabaf3d
Cliff Chen <cliffjch@usc.edu>
2024-04-08 18:39:50 -0700

    deleted commented code

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,12 +1,5 @@
 from flask import Flask
 
-# from firebase_admin import credentials, initialize_app
-
-# cred = credentials.Certificate("api/src/key.json")  # change key.json to actual one
-# default_app = initialize_app(
-#     cred, {"databaseURL": "https://dsci551---oogabooga-default-rtdb.firebaseio.com/"}
-# )
-
 
 def create_app():
     app = Flask(__name__)
